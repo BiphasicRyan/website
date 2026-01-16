233090d0b4741a3179938556facec80cd0ff6a8b
Ryan Duan <ryanduan02@gmail.com>
2026-01-16 00:04:26 -0500

    capitalize descriptions

--- src/favorite-images/images.tsx
+++ src/favorite-images/images.tsx
@@ -5,20 +5,20 @@ export const favoriteImages: FavoriteImage[] = [
     src: "432 Park Avenue.jpg",
     alt: "432 Park Avenue",
     description:
-      "NYC, 432 Park Avenue\nthe most beautiful skyscraper in the city",
+      "NYC, 432 Park Avenue\nThe most beautiful skyscraper in the city",
     // photo source: https://pixabay.com/photos/new-york-432-park-avenue-city-6750363/
   },
   {
     src: "rcp.jpeg",
     alt: "RCP",
     description:
-      "Lawence, Kansas - Sports Pavillion Lawrence\ncalm snowy night after the gym",
+      "Lawence, Kansas - Sports Pavillion Lawrence\nCalm snowy night after the gym",
     // photo source: taken by me
   },
   {
     src: "the edge.jpeg",
     alt: "The Edge",
-    description: "NYC, The Edge\nthe best view in Manhattan",
+    description: "NYC, The Edge\nThe best view in Manhattan",
     // photo source: taken by me
   },
   {
